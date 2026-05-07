041b9614181e62e3f1a6c183f530389cadedac92
lia <liajasiveleyvameza@gmail.com>
2026-05-06 22:13:48 -0700

    Lia Leyva

--- CRUD_FINAL/src/main/java/controller/ConsultaController.java
+++ CRUD_FINAL/src/main/java/controller/ConsultaController.java
@@ -1,4 +1,18 @@
 package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 
 public class ConsultaController {
+    @FXML
+    private TableColumn txtcolFecha;
+    @FXML
+    private TableColumn txtcolFolio;
+    @FXML
+    private TableColumn txtclEstado;
+    @FXML
+    private Button btnRegresar;
+
+
 }
